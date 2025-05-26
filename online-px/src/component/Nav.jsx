f6dbea6a4f3d760cd08bed7bc6fe6742a2ddec85
kimkiil <128366700+kimkiil@users.noreply.github.com>
2025-05-26 13:44:42 +0000

    feature/ nav 스타일 변경

--- online-px/src/component/Nav.jsx
+++ online-px/src/component/Nav.jsx
@@ -1,9 +1,15 @@
-export default function nav() {
+import "../component/Nav.css";
+
+export default function Nav() {
   return ( 
   <>
-    <div className="nav">
-      안녕
-    </div>
+    <nav className="menu-box">
+      <ul className="menu-list">
+        <li className="menu"><a href="#">menu1</a></li>
+        <li className="menu"><a href="#">menu2</a></li>
+        <li className="menu"><a href="#">menu3</a></li>
+      </ul>
+    </nav>
   </>
   )
 }
