537b40d7f47da5f00517959b008ec999e764ff4f
kimkiil <128366700+kimkiil@users.noreply.github.com>
2025-06-14 14:42:48 +0000

    feature/ 드롭박스 애니메이션 ver.1

--- online-px/src/component/nav-bar/DropdownMenu.jsx
+++ online-px/src/component/nav-bar/DropdownMenu.jsx
@@ -4,20 +4,19 @@ import './Nav.css';
 
 export default function DropdownMenu() {
   const [isOpen, setIsOpen] = useState(true);
-
+    
+  
+  
   const toggle = () => {
-      setIsOpen(!isOpen);
-      console.log(`${isOpen}`);
-      const menuList = document.querySelector('.menu-list');
-      if (isOpen === true) {
-          menuList.classList.add('open');
-          menuList.classList.remove('close');
-      } else if( isOpen === false) {
-          menuList.classList.add('close');
-          menuList.classList.remove('open');
-      }
+    setIsOpen(!isOpen);
+    console.log(`${isOpen}`);
+    const menuList = document.querySelector('.menu-list');
+    if (isOpen === true) {
+      menuList.classList.add('open');
+    } else if( isOpen === false) {
+      menuList.classList.remove('open');
+    }
   }
-
   return (
     <div className={"hamburger"} onClick={toggle}>
       <img src="/image/hamburger-icon.png" alt="메뉴창"/>
